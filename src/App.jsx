f3239a22202085c71866fe4ee0ec1af9c831feb3
kelson.almeida <kelson.almeida123@gmail.com>
2024-03-11 16:50:23 -0400

    att

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ function App() {
 
   return (
     <>
-    <h1>teste</h1>
       <Rotas/>
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,36 @@
+import {useState, useEffect} from "react"
+ 
+const filmesURL = import.meta.env.VITE_API;
+const chaveAPI = import.meta.env.VITE_API_KEY;
 
 function Home() {
 
+  
+  const [recente, setRecente] = useState([]);
+
+  const getRecentes = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json() //transformando o resultado em json
+
+    setRecente(data.results)
+  }  
+
+  useEffect ( () => {
+
+    const recentesFilmesURL = `${filmesURL}now_playing?language=pt-BR&${chaveAPI}`
+
+    getRecentes(recentesFilmesURL)
+  }, [])//quando a pagina iniciar ela irá ativar a função "getRecentes"
+
   return (
-    <>
-      <h1>Pagina Home</h1>
-    </>
+
+    <section class="corpoHome">
+      <h1>Recente adicionados</h1>
+
+      {recente && 
+        recente.map((filmes) => <h1>{filmes.title}</h1>)
+      }
+    </section>
   )
 }
 
