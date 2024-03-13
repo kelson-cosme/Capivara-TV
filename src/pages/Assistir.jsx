93bbd967320334b9822bcc428cf38642dbcea676
kelson.almeida <kelson.almeida123@gmail.com>
2024-03-13 15:01:52 -0400

    ajustado o player

--- src/pages/Assistir.jsx
+++ src/pages/Assistir.jsx
@@ -3,64 +3,52 @@ import { useParams } from "react-router-dom"
 
 const filmesURL = import.meta.env.VITE_API
 const chaveAPI = import.meta.env.VITE_API_KEY
-var frame = document.getElementById('SuperFlixAPIContainerVideo');
-
 
 function Assistir() {
 
-  const {id} = useParams() //pegar o paremetro id da url
-  const [filme, setFilme] = useState(null)
-
+  const id = useParams() //pegar o paremetro id da url
+  const [filme, setFilme] = useState()
   
-
-      useEffect(() => {
-
-        const getFilme = async (url) => {
-          const res = await fetch(url)
-          const data = await res.json();
-      
-          setFilme(data)
-          }
-
-        const filmeURL = `${filmesURL}${id}?${chaveAPI}`
-        getFilme(filmeURL)
-
-        console.log(filme)
-
-        // var type = "filme";
-        // var season = "";
-        // var episode = "";
+  const getFilme = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json();
+    setFilme(data)
+    }
   
-  
-        // SuperFlixAPIPluginJS(type, imdb, season, episode);
-        
-        // function SuperFlixAPIPluginJS(type, imdb, season, episode){
-        // if (type == "filme") { 
-        //   season="";episode="";
-        
-        // }else{if (season !== "") { 
-        //   season = "/"+season; 
-        //    }if (episode !== "") { 
-        //       episode = "/"+episode; 
-        //     }
-        //   }
-        //     frame.innerHTML = '<iframe src="https://superflixapi.top/'+type+'/'+imdb+season+episode+'" scrolling="no" frameborder="0" allowfullscreen="" webkitallowfullscreen="" mozallowfullscreen=""></iframe>';
-        // }
+    useEffect(() => {
+      const filmeURL = `${filmesURL}${id.id}?language=pt-BR&${chaveAPI}`
+      getFilme(filmeURL)
 
-        
-      }, []);
-    
+      let type = "filme";
+      let season = "";
+      let episode = "";
+      let imdb
 
+        if(filme != null){
+           imdb = filme.imdb_id
+        }
 
-   
+        SuperFlixAPIPluginJS(type, imdb, season, episode);
+        function SuperFlixAPIPluginJS(type, imdb, season, episode){
+        if (type == "filme") { 
+          season="";episode="";
+        }else{if (season !== "") { 
+          season = "/"+season; 
+          }if (episode !== "") { 
+            episode = "/"+episode; 
+          }}
+            var frame = document.getElementById('SuperFlixAPIContainerVideo');
+            frame.innerHTML = '<iframe sandbox="allow-scripts allow-same-origin" src="https://superflixapi.top/'+type+'/'+imdb+season+episode+'" scrolling="no" frameborder="0" allowfullscreen="" webkitallowfullscreen="" mozallowfullscreen=""></iframe>';
+        }
+    }, [filme != null]);
 
   return (
       <>
-          {filme && 
-            <h1>{filme.imdb_id}</h1>
+          {filme &&
+            <h1>{filme.title}</h1>
           }
         
-
+        <div id="SuperFlixAPIContainerVideo"></div>
       </>
     )
   }
